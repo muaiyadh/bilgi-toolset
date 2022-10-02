fa0a1c36dd241d562fefde03f0b458c95926fd24
muaiyadh <103458931+muaiyadh@users.noreply.github.com>
2022-10-02 14:01:56 +0300

    Used better method to detect page load
    Changed method to detect if first course is loaded, then click the list button. No need to change any delays now

--- 1_bilgi_learn_display_list.js
+++ 1_bilgi_learn_display_list.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bilgi Learn display as "List"
-// @version      0.1
+// @version      0.2
 // @description  Displays courses as List instead of default Cards format
 // @author       MuaiyadH
 // @match        https://learn.bilgi.edu.tr/my/*
@@ -10,26 +10,32 @@
 (function() {
     'use strict';
 
-    // Used to get elements easily using XPath
-    function getElementByXpath(path) {
-        return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    }
-
-    // Not the best method to do this, but it works fine:
-
-    // Wait until page finishes loading
-    window.addEventListener('load', function () {
-        // Aaand wait a bit more
-        setTimeout(function(){
-            let header_title_elem = getElementByXpath("//h4[@class='card-title d-inline' and text()='Course overview']");
-
-            if (header_title_elem) {
-                let list_button_elem = getElementByXpath("//a[@data-value='list']");
-                if (list_button_elem) {
-                    console.log("Found button. Will click.");
-                    list_button_elem.click();
-                }
+    // Source: https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+    function waitForElm(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
             }
-        }, 750); // If this doesnt work, try increasing this delay
+
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    resolve(document.querySelector(selector));
+                    observer.disconnect();
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
+    let listButtonCSS = ''; // The List button CSS selector that's used to actually change
+    let firstCourseCSS = ''; // Used as an indicator that the page has fully loaded
+    waitForElm('ul.dropdown-menu-right > li:nth-child(2) > a:nth-child(1)').then((elem) => {
+        waitForElm('div.slick-list:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > figure:nth-child(1) > figcaption:nth-child(3)').then((elem2) => {
+            elem.click();
+            console.log('Hi');
+        });
     });
 })();
